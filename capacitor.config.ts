48d1efa7fe560abbaddb6e8282750f09957fe5b0
Axel Schulz <1116730+noheadx@users.noreply.github.com>
2025-09-04 16:39:39 +0200

    Update capacitor.config.ts corrected appID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.5bdd19a62450429e92588b968c43ad4d',
+  appId: 'app.lovable.breathebuddytracker',
   appName: 'breathe-buddy-tracker',
   webDir: 'dist',
-  // Remove server config for production build to use local files
-  // server: {
-  //   url: "https://5bdd19a6-2450-429e-9258-8b968c43ad4d.lovableproject.com?forceHideBadge=true",
-  //   cleartext: true
-  // },
   plugins: {
     SplashScreen: {
       launchShowDuration: 2000,
